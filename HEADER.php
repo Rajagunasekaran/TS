96d5a8e37fe807fdbc3704eb97f647cde2d55f31
lalitharajendiran <lalitha.rajendiran@ssomens.com>
2015-02-27 18:39:49 +0530

    1.Latest Jquery links Updated nd Data Table.

--- HEADER.php
+++ HEADER.php
@@ -1,13 +1,13 @@
 <html>
 <!--HEAD TAG START-->
 <head>
-    <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css">
+    <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/themes/smoothness/jquery-ui.css">
     <!--TO SUPPORT JQUERY FUNCTIONALITIES-->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <!--TO SUPPORT JQUERY UI FUNCTIONALITIES-->
-    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js"></script>
     <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-    <script type="text/javascript" src="//cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="//cdn.datatables.net/1.10.5/js/jquery.dataTables.min.js"></script>
 
     <!--TO SUPPORT JQUERY LIB VALIDATION-->
     <script src="jquery/JQuery.js"></script>
@@ -22,16 +22,11 @@
     <link rel="stylesheet" type="text/css" href="https://googledrive.com/host/0B_f0d7mdbV_USEZYZDVnRUZMZW8/dataTables.tableTools.css">
 
     <style type="text/css" class="init">
-
         div.container {
             width: 80%;
         }
-
     </style>
 
 
-    <script type="text/javascript" language="javascript" src="https://googledrive.com/host/0B_f0d7mdbV_USEZYZDVnRUZMZW8/DataTable1.10.4.js"></script>
+    <script type="text/javascript" language="javascript" src="https://googledrive.com/host/0B_f0d7mdbV_USEZYZDVnRUZMZW8/DataTable1.10.5.js"></script>
     <script type="text/javascript" language="javascript" src="https://googledrive.com/host/0B_f0d7mdbV_USEZYZDVnRUZMZW8/dataTables.tableTools.js"></script>
-
-
-
